a503ff7f25aa44b6ffc8a96a27c19c095cedae27
Yuanyuan Li(Alexis) <98563857+Alexiscode1@users.noreply.github.com>
2022-05-12 17:05:19 -0700

    Add files via upload

---
+++ hw02.py
@@ -0,0 +1,136 @@
+"""Homework 2."""
+
+def fib(n):
+    """Returns the nth Fibonacci number.
+
+    >>> fib(0)
+    0
+    >>> fib(1)
+    1
+    >>> fib(2)
+    1
+    >>> fib(3)
+    2
+    >>> fib(4)
+    3
+    >>> fib(5)
+    5
+    >>> fib(6)
+    8
+    >>> fib(100)
+    354224848179261915075
+    """
+    f0 =0
+    f1 =1
+    i =1
+    if n ==0:
+        return f0
+    elif n ==1:
+        return f1
+    else:
+        while i < n:
+            f2 = f0+f1
+            f0=f1
+            f1=f2
+            i= i+1
+        return f2
+
+
+def nonzero(lst):
+    """ Returns the first nonzero element of a list
+
+    >>> nonzero([1, 2, 3])
+    1
+    >>> nonzero([0, 1, 2])
+    1
+    >>> nonzero([0, 0, 0, 0, 0, 0, 5, 0, 6])
+    5
+    """
+    return [i for i in lst if i!=0][0]
+
+
+def has_n(lst, n):
+    """ Returns whether or not a list contains the value n.
+
+    >>> has_n([1, 2, 2], 2)
+    True
+    >>> has_n([0, 1, 2], 3)
+    False
+    >>> has_n([], 5)
+    False
+    """
+    if n in lst:
+        return True
+    else:
+        return False
+
+
+def total_price(prices):
+    """
+    Finds the total price of all products in prices including a
+    50% tax on products with a price greater than or equal to 20.
+    >>> total_price([5, 20, 30, 7])
+    87
+    >>> total_price([8, 4, 3])
+    15
+    >>> total_price([10, 100, 4])
+    164
+    """
+    
+    total = 0
+    for i in prices:
+        if i >=20:
+            i= i*1.5
+            total = total + i
+        else:
+            total = total+ i
+    return int(total)
+
+def arange(start, end, step=1):
+    """
+    arange behaves just like np.arange(start, end, step).
+    You only need to support positive values for step.
+
+    >>> arange(1, 3)
+    [1, 2]
+    >>> arange(0, 25, 2)
+    [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24]
+    >>> arange(999, 1231, 34)
+    [999, 1033, 1067, 1101, 1135, 1169, 1203]
+
+    """
+    lst = []
+    while start < end:
+        lst.append(start)
+        start = start+step
+        
+    return lst
+    
+
+
+def reverse_iter_for(lst):
+    """Returns the reverse of the given list.
+
+    >>> reverse_iter_for([1, 2, 3, 4])
+    [4, 3, 2, 1]
+    """
+    rev_lst=[]
+    
+    for i in range(len(lst)):
+        rev_lst.append(lst[-(i+1)])
+        i = i+1
+        
+    return rev_lst
+
+def reverse_iter_while(lst):
+    """Returns the reverse of the given list.
+
+    >>> reverse_iter_while([1, 2, 3, 4])
+    [4, 3, 2, 1]
+    """
+    rev_lst, i = [], 0
+    while i < len(lst):
+        rev_lst.append(lst[-(i+1)])
+        i = i+1
+    return rev_lst
+
