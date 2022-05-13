88b0fe19651a6be3a87866a2f49deb54fe3f20be
Yuanyuan Li(Alexis) <98563857+Alexiscode1@users.noreply.github.com>
2022-05-12 17:21:27 -0700

    Add files via upload

---
+++ hw09.py
@@ -0,0 +1,159 @@
+# Linked List Class
+class Link:
+    """
+    >>> s = Link(1, Link(2, Link(3)))
+    >>> s
+    Link(1, Link(2, Link(3)))
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest is not Link.empty:
+            rest_str = ', ' + repr(self.rest)
+        else:
+            rest_str = ''
+        return 'Link({0}{1})'.format(repr(self.first), rest_str)
+
+    def __len__(self):
+        """ Return the number of items in the linked list.
+
+        >>> s = Link(1, Link(2, Link(3)))
+        >>> len(s)
+        3
+        >>> s = Link.empty
+        >>> len(s)
+        0
+        """
+        return 1 + len(self.rest)
+
+    def __getitem__(self, i):
+        """Returning the element found at index i.
+
+        >>> s = Link(1, Link(2, Link(3)))
+        >>> s[1]
+        2
+        >>> s[2]
+        3
+        """
+        if i == 0:
+            return self.first
+        else:
+            return self.rest[i-1]
+
+def print_link(link):
+    """Print elements of a linked list link.
+
+    >>> link = Link(1, Link(2, Link(3)))
+    >>> print_link(link)
+    <1 2 3>
+    >>> link1 = Link(1, Link(Link(2), Link(3)))
+    >>> print_link(link1)
+    <1 <2> 3>
+    >>> link1 = Link(3, Link(Link(4), Link(5, Link(6))))
+    >>> print_link(link1)
+    <3 <4> 5 6>
+    """
+    print('<' + helper(link).rstrip() + '>')
+
+def helper(link):
+    if link == Link.empty:
+        return ''
+    elif isinstance(link.first, Link):
+        return '<' + helper(link.first).rstrip() + '> ' + helper(link.rest)
+    else:
+        return str(link.first) +' '+  helper(link.rest)
+
+
+
+######################
+#### Linked Lists ####
+######################
+
+def link_to_list(link):
+    """Takes a Link and returns a Python list with the same elements.
+
+    >>> link = Link(1, Link(2, Link(3, Link(4))))
+    >>> link_to_list(link)
+    [1, 2, 3, 4]
+    >>> link_to_list(Link(88))
+    [88]
+    >>> link_to_list(Link.empty)
+    []
+    """
+    if link==Link.empty:
+        return []
+    else:
+        return [link.first]+link_to_list(link.rest)
+
+
+def every_other(s):
+    """Mutates a linked list so that all the odd-indiced elements are removed
+    (using 0-based indexing).
+
+    >>> s = Link(1, Link(2, Link(3, Link(4))))
+    >>> every_other(s)
+    >>> s
+    Link(1, Link(3))
+    >>> odd_length = Link(5, Link(3, Link(1)))
+    >>> every_other(odd_length)
+    >>> odd_length
+    Link(5, Link(1))
+    >>> singleton = Link(4)
+    >>> every_other(singleton)
+    >>> singleton
+    Link(4)
+    """
+    if len(s) <2:
+        return
+    else:
+        s.rest = s.rest.rest
+        every_other(s.rest)
+           
+
+def slice_link(link, start, end):
+    """Slices a Link from start to end (as with a normal Python list).
+
+    >>> link = Link(3, Link(1, Link(4, Link(1, Link(5, Link(9))))))
+    >>> new = slice_link(link, 1, 4)
+    >>> print_link(new)
+    <1 4 1>
+    >>> link2 = slice_link(Link(1), 0, 1)
+    >>> print_link(link2)
+    <1>
+    >>> link3 = slice_link(Link.empty, 0, 0)
+    >>> print_link(link3)
+    <>
+    """
+    if end==0:
+        return Link.empty
+    elif start==0:
+        return Link(link.first, slice_link(link.rest,0,end-1))
+    else:
+        return slice_link(link.rest, start-1, end-1)
+
+
+def deep_map(f, link):
+    """Return a Link with the same structure as link but with fn mapped over
+    its elements. If an element is an instance of a linked list, recursively
+    apply f inside that linked list as well.
+
+    >>> s = Link(1, Link(Link(2, Link(3)), Link(4)))
+    >>> print_link(deep_map(lambda x: x * x, s))
+    <1 <4 9> 16>
+    >>> print_link(s) # unchanged
+    <1 <2 3> 4>
+    >>> print_link(deep_map(lambda x: 2 * x, Link(s, Link(Link(Link(5))))))
+    <<2 <4 6> 8> <<10>>>
+    """
+    if link==Link.empty:
+        return link
+    elif isinstance(link.first, Link):
+        return Link(deep_map(f, link.first), deep_map(f, link.rest))
+    else:
+        return Link(f(link.first), deep_map(f, link.rest))
+
